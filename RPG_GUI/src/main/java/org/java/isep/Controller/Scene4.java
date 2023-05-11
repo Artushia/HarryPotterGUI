59e7e992711ad281c136b04265fd106c07af6ab3
Artushia <arthurterisse@gmail.com>
2023-05-11 15:08:07 +0200

    Edit of the final label. Works fine

--- RPG_GUI/src/main/java/org/java/isep/Controller/Scene4.java
+++ RPG_GUI/src/main/java/org/java/isep/Controller/Scene4.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Label;
 public class Scene4 {
 
     @FXML
-    Label endphrase;
+    public Label endphrase;
 
     public void displayEndMessage(String phrase) {
         endphrase.setText("You " + phrase + " the game !");
